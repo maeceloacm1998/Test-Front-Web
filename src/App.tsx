677396890b4b5b91775afe6cf662a8954104e326
Felipe Gonzalez <felipecgonzalez2305@gmail.com>
2025-05-04 16:03:48 -0300

    feat: criado tela cardscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppRoutes from './routes';
 import './App.css';
+import { CartScreen } from './components/CartScreen/cartScreen';
+
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
-      <AppRoutes />
+      <CartScreen />
+      <h2>Hello World</h2>
     </div>
   );
 };
---
+++ src/components/CartScreen/cartscreen.tsx
@@ -0,0 +1,8 @@
+
+export function CartScreen() {
+    return (
+        <header className="tex-red-500">
+            <h1>Bag</h1>
+        </header>
+    )
+}
