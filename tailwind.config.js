376a72e2e6ddb5faf766ff03c68dce48c35e705b
Marcelo Mendes <maeceloacm1998@gmail.com>
2025-05-04 16:41:03 -0300

    chore: add tallwind and cofigure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -22,4 +22,4 @@ export default {
     },
   },
   plugins: [],
-} 
+}
