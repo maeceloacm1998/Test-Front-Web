f143e632025d76836c4f0654d275d4975d6c4321
Felipe Gonzalez <felipecgonzalez2305@gmail.com>
2025-05-11 19:57:29 -0300

    feat: create component and list products

---
+++ src/components/ProductList/type.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    name: string;
+    price?: number;
+    price_original?: number;
+    price_discounted?: number;
+    quantity: number;
+    image_url: string;
+    product_url?: string;
+}
+
